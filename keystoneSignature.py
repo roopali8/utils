8093f649b2b4b1cc318798025dd25efbeba1fa03
Roopali <ruchi8.iitr@gmail.com>
2015-12-18 11:32:25 +0530

    Update keystoneSignature.py

--- keystoneSignature.py
+++ keystoneSignature.py
@@ -3,6 +3,7 @@ import hashlib
 import hmac
 
 ##rrwat: generated from keystone
+#url is you url after the base url-> http://localhost:5000/v3/create_user?xyz=abc will be create_user?xyz=abc
 def get_signature(url, secret):
     import pdb; pdb.set_trace()
     msg = base64.urlsafe_b64decode(str(url))
